03a522b52e5897699df073d7c372c0dff36e5cdb
Chun Sreymech <chunsreymech92@gmail.com>
2026-08-24 19:57:07 +0700

    c

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::view('/docs', 'api-docs')->name('api.docs');
+
+Route::get('/docs/openapi.json', function () {
+    return response()->json([
+        'openapi' => '3.0.3',
+        'info' => ['title' => 'E-Commerce REST API', 'version' => '1.0.0'],
+        'servers' => [['url' => url('/api/v1')]],
+        'paths' => collect(Route::getRoutes())->filter(fn ($route) => str_starts_with($route->uri(), 'api/v1/'))->mapWithKeys(function ($route) {
+            $path = '/'.str_replace('api/v1/', '', $route->uri());
+            $methods = collect($route->methods())->reject(fn ($method) => $method === 'HEAD');
+            return [$path => $methods->mapWithKeys(fn ($method) => [strtolower($method) => ['responses' => ['200' => ['description' => 'JSON response']]]])->all()];
+        })->all(),
+    ]);
+})->name('api.docs.openapi');
